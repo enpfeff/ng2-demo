1a0776f5e0d6f46649b0c592f7357e05812494f0
Ian <ian.pfeff@gmail.com>
2016-07-08 11:49:05 -0600

    add in Type as TS was not happy

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, Type} from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Ng 2 App</h1>'
 })
-export class AppComponent {
+export class AppComponent extends Type {
 }
